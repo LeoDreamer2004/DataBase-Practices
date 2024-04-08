164e96c285e7b607c18970cf633722d84d3c1a5a
LeoDreamer <110815661+LeoDreamer2004@users.noreply.github.com>
2024-04-09 00:24:32 +0800

    Update database

--- 01/utils/data_helper.py
+++ 01/utils/data_helper.py
@@ -1,4 +1,6 @@
 from pymysql.cursors import Cursor
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def fetch_cursor(cursor: Cursor):
@@ -16,3 +18,15 @@ def fetch_cursor(cursor: Cursor):
     description = cursor.description
     data = cursor.fetchall()
     return [dict(zip([x[0] for x in description], row)) for row in data]
+
+
+URL = "mysql+pymysql://practice:123456@127.0.0.1:3306/practice1"
+
+
+def csv2sql(file_path: str, table_name: str):
+    """
+    Read a csv file and write it to a table in the database.
+    """
+    engine = create_engine(URL)
+    df = pd.read_csv(file_path)
+    df.to_sql(table_name, engine, if_exists="append", index=False)
